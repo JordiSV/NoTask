6796401a7d4efe3b1fdb30b3ff4559fafef507b6
z <zv>
2021-10-28 16:26:39 +0200

    first commit

---
+++ src/test/java/com/example/notask/NotaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.notask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotaskApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
